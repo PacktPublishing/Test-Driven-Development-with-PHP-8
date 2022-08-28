c6e3c7de349a39b687c43898f29a170d32bd8593
Rainier Sarabia <rainier.sarabia@astutepayroll.com>
2022-08-28 11:34:21 +1000

    Chapter 7 - Dev Env Base
    Chapter 7 - Dev Env Base

---
+++ Chapter 7/base_codes/phptdd/codebase/symfony/src/Entity/Consumption.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ConsumptionRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ConsumptionRepository::class)]
+class Consumption
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 50)]
+    private $name;
+
+    #[ORM\Column(type: 'integer')]
+    private $total;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getTotal(): ?int
+    {
+        return $this->total;
+    }
+
+    public function setTotal(int $total): self
+    {
+        $this->total = $total;
+
+        return $this;
+    }
+}
