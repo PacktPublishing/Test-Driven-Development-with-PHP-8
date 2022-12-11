e24b8d2a6d8a6b7dfb3d09bc8c39e0c876915d64
Rainier Sarabia <raindownunder@gmail.com>
2022-12-11 11:12:18 +1100

    Demo Setup Update
    Demo Setup Update

--- Chapter 6/complete/demoSetup.sh
+++ Chapter 6/complete/demoSetup.sh
@@ -2,4 +2,6 @@
 docker "kill $(docker ps -q)"
 docker-compose -f ./phptdd/docker/docker-compose.yml build
 docker-compose -f ./phptdd/docker/docker-compose.yml up -d
+
+#List All Containers
 docker ps
